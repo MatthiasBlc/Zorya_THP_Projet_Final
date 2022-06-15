322419c8db16912fc048d129e8dd538d2d7713b8
Olivia <oliviarudolf67@gmail.com>
2022-06-15 16:14:48 +0200

    modify manifest for assets

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,19 @@
 //= link_tree ../images
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
+
+//= link spinkit.css
+//= link perfect-scrollbar.css
+//= link material-icons.css
+//= link fontawesome.css
+//= link app.css
+//= link dark.css
+
+//= link jquery.min.js
+//= link popper.min.js
+//= link bootstrap.min.js
+//= link perfect-scrollbar.js
+//= link dom-factory.js
+//= link material-design-kit.js
+//= link app.js
+
