4d23fddb4825e6a78cb8e246cce1e257e85043a3
Iris <marlx42@protonmail.com>
2022-06-15 21:03:28 +0200

    correction destination mail

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -3,7 +3,7 @@ class ContactMailer < ApplicationMailer
   def contact_mail(contact)
     @contact = contact
     @url = 'https://zorya-learning-dev.herokuapp.com/'
-    mail(to: 'zorya.learning@gmail.com', subject: 'Prise de contact via le site')
+    mail(to: 'zorya.learning@yopmail.com', subject: 'Prise de contact via le site')
   end
 
 
