76c54d3dca2a5626b3631ff1636c13df096d51e6
MatthiasBlc <mat.bouloc@gmail.com>
2022-06-16 12:51:24 +0200

    setup the create method for assignments

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -6,4 +6,26 @@ class AssignmentsController < ApplicationController
   def show
     @assignment = Assignment.find(params[:id])
   end
+
+  def new
+    @assignment = Assignment.new
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @learning = Learning.find(params[:learning_id])
+    puts '%'*120
+    puts @user.id
+    puts @learning.id
+    puts '%'*120
+
+    @assignment = Assignment.create(user_id: @user.id, learning_id: @learning.id)
+
+    respond_to do |format|
+      format.html { redirect_to assignments_path, notice: 'inscription réussie' }
+      format.js {}
+    end
+  end
+
+
 end
