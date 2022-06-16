b406a1227f844b4a2bf328907395d7c14ee02d6e
Iris <marlx42@protonmail.com>
2022-06-17 01:04:22 +0200

    add button validation learning in show

--- app/controllers/learnings_controller.rb
+++ app/controllers/learnings_controller.rb
@@ -6,5 +6,6 @@ class LearningsController < ApplicationController
   
   def show
     @learning = Learning.find(params[:id])
+    @assignment = Assignment.find_by(learning_id: @learning.id, user_id: current_user.id)
   end
 end
