937caf8f8a8e6520afafe59586f1c3543a579d61
MatthiasBlc <mat.bouloc@gmail.com>
2022-06-16 11:53:39 +0200

    adding element for show in the compangies controller

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,4 +1,9 @@
 class CompaniesController < ApplicationController
   def show
+    @company = Company.find(params[:id])
+    @users = @company.users
+    @learnings = @company.learnings
+    # @offers = @company.offers
+    @offers = Offer.all
   end
 end
