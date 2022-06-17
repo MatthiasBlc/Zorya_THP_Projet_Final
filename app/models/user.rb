f68170532cca1cf836dff62fa5101fafb990e88d
MatthiasBlc <mat.bouloc@gmail.com>
2022-06-17 09:55:57 +0200

    update std enum for being a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,8 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "Veuillez saisir un email valide s'il vous plaît." }
 
   enum status: {
-    customer: 0,
-    user: 1,
+    user: 0,
+    customer: 1,
     administrator: 2
   }, _prefix: true
 
