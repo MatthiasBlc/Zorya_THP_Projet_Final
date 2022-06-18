640484e5e4c58f296d3ffc3f4f1deb1f90d6dda7
MatthiasBlc <mat.bouloc@gmail.com>
2022-06-18 11:31:54 +0200

    models cleanup

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -2,10 +2,8 @@ class Assignment < ApplicationRecord
   belongs_to :learning
   belongs_to :user
 
-
   after_create :inscription_send
   after_update :validation_send
-  
 
   def inscription_send
     UserMailer.new_assignment_email(self).deliver_now
@@ -14,6 +12,4 @@ class Assignment < ApplicationRecord
   def validation_send
     UserMailer.validation_assignment_email(self).deliver_now
   end
-
-
 end
