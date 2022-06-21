b7c68e5caad0a64db44f5a00689b4b81490f109d
Iris <marlx42@protonmail.com>
2022-06-21 17:08:58 +0200

    corrections

--- app/models/user_company.rb
+++ app/models/user_company.rb
@@ -5,12 +5,7 @@ class UserCompany < ApplicationRecord
   after_create :slack_welcome
 
   def slack_welcome
-    begin
       slack_notifier(self.company.webhook, self.company.channel).ping "Hello #{self.user.email}, tu viens d'être ajouté au channel Slack de #{self.company.name}, c'est ici que tu recevras tes notifications pour tes formations Zorya !"
-    rescue => exception
-      flash.now[:alert] = "Votre connectique Slack est mal paramétrée, veuillez contacter votre Gestionnaire de compte afin de résoudre votre problème."
-    end
-    
   end
 
 end
