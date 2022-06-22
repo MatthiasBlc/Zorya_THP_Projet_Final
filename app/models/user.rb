04aa1945b6bf7762fb29b4ae605a6755a42fb9a6
Iris <marlx42@protonmail.com>
2022-06-22 23:02:24 +0200

    add rules in model user and explanation in page new

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,8 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true,
                     format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "Veuillez saisir un email valide s'il vous plaît." }
 
+  validate :password_complexity
+
   enum status: {
     user: 0,
     customer: 1,
@@ -20,6 +22,12 @@ class User < ApplicationRecord
 
   def welcome_send
     UserMailer.welcome_email(self).deliver_now
+  end
+  
+  def password_complexity
+    return if password.blank? || password =~ /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,70}$/
+
+    errors.add :password, 'Votre mot de passe doit contenir minimum 8 caractères, 1 majuscule, 1 minuscule, 1 chiffre et 1 caractère spécial (#?!@$%^&*-).'
   end
 
 end
