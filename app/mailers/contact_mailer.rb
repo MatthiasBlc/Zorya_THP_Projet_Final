55507cb49df8944b2e868e96c352af4290ad65a9
MatthiasBlc <mat.bouloc@gmail.com>
2022-06-23 15:55:29 +0200

    corrections on mailers

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,11 +1,9 @@
 class ContactMailer < ApplicationMailer
   default from: 'zorya.learning@gmail.com'
-  
+
   def contact_mail(contact)
     @contact = contact
     @url = 'https://zorya-learning.herokuapp.com/'
     mail(to: 'zorya.learning@yopmail.com', subject: 'Prise de contact via le site')
   end
-
-
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,9 +4,9 @@ class UserMailer < ApplicationMailer
   def welcome_email(user)
     @user = user
     @email = 'zorya.learning@gmail.com'
-    @website ='https://zorya-learning.herokuapp.com/'
+    @website = 'https://zorya-learning.herokuapp.com/'
     @url = 'https://zorya-learning.herokuapp.com/users/sign_in'
-    mail(to: @user.email, subject: 'Bienvenu sur Zorya')
+    mail(to: @user.email, subject: 'Bienvenue sur Zorya')
   end
 
   def new_assignment_email(assignment)
@@ -14,7 +14,7 @@ class UserMailer < ApplicationMailer
     @user = @assignment.user
     @url = 'https://zorya-learning.herokuapp.com/users/sign_in'
     @email = 'zorya.learning@gmail.com'
-    @website ='https://zorya-learning.herokuapp.com/'
+    @website = 'https://zorya-learning.herokuapp.com/'
     mail(to: @user.email, subject: 'Zorya - Tu viens de recevoir une nouvelle formation')
   end
 
@@ -22,8 +22,7 @@ class UserMailer < ApplicationMailer
     @assignment = assignment
     @user = @assignment.user
     @email = 'zorya.learning@gmail.com'
-    @website ='https://zorya-learning.herokuapp.com/'
+    @website = 'https://zorya-learning.herokuapp.com/'
     mail(to: @user.email, subject: 'Zorya - Bravo formation validée')
   end
-
 end
